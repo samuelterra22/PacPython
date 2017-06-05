139944647008b2da4c4c330c56f53b447a266e21
calixto <calixtinn@gmail.com>
2017-06-05 17:22:02 -0300

    Tela preta implementada

---
+++ Main.py
@@ -0,0 +1,23 @@
+#Para rodar deve-se baixar a biblioteca pygame:
+
+# sudo pip install pygame
+
+import pygame, sys, random
+from pygame.locals import *
+
+w = 800
+h = 480
+z = 0
+
+screen = pygame.display.set_mode((w,h))
+pygame.display.flip()
+clock = pygame.time.Clock()
+
+while 1:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((0,0,0))
+    pygame.display.update()
