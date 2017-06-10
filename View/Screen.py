53791da60687188c80cd460ea86a75cbb69fd1b2
Matheus Calixto <calixtinn@gmail.com>
2017-06-10 15:58:58 -0300

    Bordas criadas, e limites implementados

--- View/Screen.py
+++ View/Screen.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pygame, sys, random
 from pygame.locals import *
 
@@ -13,41 +15,101 @@ yellow = (255,255,0)
 change_rate = 10
 tick = 13
 
-gameDisplay = pygame.display.set_mode((800, 600))
+# Dimensões da Tela
+
+width = 800
+height = 600
+
+# Dimensões do pacman e da borda
+
+pacman_radius = 15
+border_width = 5
+
+# Limite das bordas do labirinto
+
+boundarie_maze = pacman_radius + border_width
+
+# Limites até onde o pacman pode alcançar
+
+boundarie_x_pacman = width - boundarie_maze
+boundarie_y_pacman = height - boundarie_maze
+
+gameDisplay = pygame.display.set_mode((width, height))
 pygame.display.set_caption("PacPython \o/")
 
+# Comando de parada do jogo
 gameExit = False
 
-lead_x = 300
-lead_y = 300
-lead_x_change = 0
-lead_y_change = 0
+# Posições iniciais do Pac-Man
+
+pacman_x = boundarie_maze
+pacman_y = height - boundarie_maze
+
+# Variáveis para a mudança de posição do pacman
+
+pacman_x_change = 0
+pacman_y_change = 0
+
+# Clock para definir os Frames por segundo
 
 clock = pygame.time.Clock()
+
+# Desenha as bordas
+
+pygame.draw.line(gameDisplay, white, (0, 0),(0, height), border_width)
+pygame.draw.line(gameDisplay, white, (0, 0), (width, 0), border_width)
+pygame.draw.line(gameDisplay, white, (0, height), (width, height), border_width)
+pygame.draw.line(gameDisplay, white, (width, 0), (width, height), border_width)
+
+# Atualiza a tela
+
+pygame.display.update()
+
+# Looping principal do jogo
 
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
-
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change = -change_rate
-                lead_y_change = 0
-            if event.key == pygame.K_RIGHT:
-                lead_x_change = change_rate
-                lead_y_change = 0
-            if event.key == pygame.K_UP:
-                lead_y_change = -change_rate
-                lead_x_change = 0
-            if event.key == pygame.K_DOWN:
-                lead_y_change = change_rate
-                lead_x_change = 0
+                pacman_x_change = -change_rate
+                pacman_y_change = 0
+            elif event.key == pygame.K_RIGHT:
+                pacman_x_change = change_rate
+                pacman_y_change = 0
+            elif event.key == pygame.K_UP:
+                pacman_y_change = -change_rate
+                pacman_x_change = 0
+            elif event.key == pygame.K_DOWN:
+                pacman_y_change = change_rate
+                pacman_x_change = 0
 
-    lead_x += lead_x_change
-    lead_y += lead_y_change
+    # Atualiza a posição do pacman
+
+    pacman_x += pacman_x_change
+    pacman_y += pacman_y_change
+
+    # Verifica as bordas.
+
+    if pacman_x > boundarie_x_pacman:
+        pacman_x = boundarie_x_pacman
+    elif pacman_x < boundarie_maze:
+        pacman_x = boundarie_maze
+
+    if pacman_y > boundarie_y_pacman:
+        pacman_y = boundarie_y_pacman
+    elif pacman_y < boundarie_maze:
+        pacman_y = boundarie_maze
+
+    # Pinta o fundo da tela de preto
+
     gameDisplay.fill(black)
-    pygame.draw.circle(gameDisplay, yellow, (lead_x,lead_y), 15, 0)
+
+    # Desenha o Pacman
+
+    pygame.draw.circle(gameDisplay, yellow, (pacman_x,pacman_y), pacman_radius, 0)
+
     pygame.display.update()
 
     # FPS
